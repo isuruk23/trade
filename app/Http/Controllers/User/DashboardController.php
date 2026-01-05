7eb00f7cecc544385bb10d81b9abfb998bfde3fe
isuruk23 <isuru.k23@gmail.com>
2026-01-05 10:17:13 +0530

    trading-controller-v1

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -6,15 +6,17 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Transaction;
+use App\Models\Wallet;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $user = auth()->user();
-
+ 
+        
         return view('user.dashboard', [
-            'balance' => $user->balance,
+            'balance' => Wallet::where('user_id',$user->id)->pluck('balance')->first(),
             'deposits' => $user->transactions()->where('type','deposit')->count(),
             'withdrawals' => $user->transactions()->where('type','withdrawal')->count(),
             'pending' => $user->transactions()->where('status','pending')->count(),
